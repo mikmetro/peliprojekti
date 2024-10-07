b7c5469711ab5f9802a8fda848ef41fa903b7fdc
petja_pajunen <petja.pajunen@elisanet.fi>
2024-10-07 18:11:59 +0300

    commit

--- classes/player.py
+++ classes/player.py
@@ -2,7 +2,12 @@ import json
 from .airport import *
 from .upgrades import *
 from constants import GAME_TICK
+from constants import CO2_BUDGET
+from constants import SECURITY_BASE
+import random
+
 import os.path
+
 
 class Player:
     def __init__(self, name: str, airports: list[AirPort]) -> None:
@@ -15,7 +20,7 @@ class Player:
             "airport_price_len": 0
         }
         self.available_airports: list[AirPort] = airports
-        
+
     def create_player(self, airports: list[AirPort]):
         if os.path.isfile(f"profiles/{self.name}.json"):
             try:
@@ -27,11 +32,15 @@ class Player:
                     for i in user_data["airports"]:
                         airport = user_data["airports"][i]
                         ups = [airport["upgrades"][j] for j in range(3)]
-                        ups = (IncomeUpgrade(*ups[0].values()), Co2Upgrade(*ups[1].values()), SecurityUpgrade(*ups[2].values()))
-                        self.airports.append(AirPort(airport['id'], i, airport["country"], airport["price"], airport["co2_generation"], ups))
+                        ups = (IncomeUpgrade(
+                            *ups[0].values()), Co2Upgrade(*ups[1].values()), SecurityUpgrade(*ups[2].values()))
+                        self.airports.append(AirPort(
+                            airport['id'], i, airport["country"], airport["price"], airport["co2_generation"], ups))
                     self.cache = user_data["cache"]
-                    PLAYER_AIRPORTS = [airport.name for airport in self.airports]
-                    AVAILABLE_AIRPORTS: list[AirPort] = [i for i in airports if i.name not in PLAYER_AIRPORTS]
+                    PLAYER_AIRPORTS = [
+                        airport.name for airport in self.airports]
+                    AVAILABLE_AIRPORTS: list[AirPort] = [
+                        i for i in airports if i.name not in PLAYER_AIRPORTS]
                     self.available_airports = AVAILABLE_AIRPORTS
             except:
                 return "Failed to load profile"
@@ -40,21 +49,44 @@ class Player:
                 self.save_profile()
             except:
                 return "Failed to create profile"
-          
+
     # Etene 1 peli tick
     def tick(self) -> None:
         for i in self.airports:
             self.money += i.upgrades[0].get_effect() * GAME_TICK
-            self.co2_used = max(0, self.co2_used + (i.co2_generation - i.upgrades[1].get_effect()) * GAME_TICK)
+            self.co2_used = max(
+                0, self.co2_used + (i.co2_generation - i.upgrades[1].get_effect()) * GAME_TICK)
 
-    # Tämän funktion voi laittaa joskus purchase_airport funktioon
+            if self.co2_used > CO2_BUDGET:                      # Sakko pitää vielä muuttaa
+                if self.money - 1000 < 1:
+                    print("Sait sakon ilmasto rikkeestä.")
+                    self.money = 0
+                else:
+                    self.money = self.money-1000
+                    print("Sait sakon ilmasto rikkeestä.")
+
+            if random.random() < (SECURITY_BASE + SECURITY_BASE * (i.upgrades[2].get_effect()/100))/100:         # if Random value < (Base + (-Base) * (security/100)) / 100
+                vakavuus = random.randint(1,100)
+                if vakavuus < 3 and len(self.airports) > 1:
+                    self.airports.pop(-1)
+                    print("Menetit lentokentän vakavasta turvallisuus rikkeestä.")
+                elif self.money - 3000 < 1:
+                    self.money = 0
+                    print("Sait sakon turvallisuus rikkeestä.")
+                else:
+                    self.money = self.money - 3000
+                    print("Sait sakon turvallisuus rikkeestä.")
+                # Tämän funktion voi laittaa joskus purchase_airport funktioon
+
     def give_airport(self, airport: AirPort) -> tuple[bool, str]:
         if airport.name in self.airports:
             return (False, "Airport already owned")
 
         self.airports.append(airport)
-        self.cache["airport_max_len"] = max(len(airport.name), self.cache["airport_max_len"])
-        self.cache["airport_price_len"] = max(len(str(airport.price)), self.cache["airport_price_len"])
+        self.cache["airport_max_len"] = max(
+            len(airport.name), self.cache["airport_max_len"])
+        self.cache["airport_price_len"] = max(
+            len(str(airport.price)), self.cache["airport_price_len"])
 
         self.remove_from_available(airport)
         return (True, "Airport given")
@@ -67,8 +99,10 @@ class Player:
 
         self.money -= airport.price
         self.airports.append(airport)
-        self.cache["airport_max_len"] = max(len(airport.name), self.cache["airport_max_len"])
-        self.cache["airport_price_len"] = max(len(str(airport.price)), self.cache["airport_price_len"])
+        self.cache["airport_max_len"] = max(
+            len(airport.name), self.cache["airport_max_len"])
+        self.cache["airport_price_len"] = max(
+            len(str(airport.price)), self.cache["airport_price_len"])
 
         self.remove_from_available(airport)
         return (True, "Purchase successful")
@@ -85,7 +119,7 @@ class Player:
         try_upgrade = upgrade.upgrade()
         if try_upgrade[0] == False:
             return try_upgrade
-            
+
         self.money -= upgrade.get_price()
 
         return (True, "Purchase successful")
--- constants.py
+++ constants.py
@@ -6,5 +6,6 @@ RIGHT = "\x1b[1D"
 CLR = "\x1b[0K"
 TOP = "\x1b[0;0H"
 CO2_BUDGET: int = 100000  # Jos pelaaja päästää enemmän co2 kuin tämä luku häviät pelin
+SECURITY_BASE: int = 2
 AUTOSAVE_DELAY: int = 5
 AIRPORT_MENU_TABS = ["Inventory", "Shop"]
